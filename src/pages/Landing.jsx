ba0d303f67c96a930b386f4181d4d72557bbc94a
ahmadadptr001 <ahmadadptr@gmail.com>
2025-06-27 17:39:05 +0700

    testing opacity

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -33,19 +33,19 @@ const Landing = () => {
                         absolute top-1/2 left-0 w-max -translate-y-1/2
                         -translate-x-[42%] rotate-90 group-hover:text-warning 
                         text-white px-2 py-1 font-medium duration-1000
-                        whitespace-nowrap text-[20px] xs:text-[32px]
+                        whitespace-nowrap text-[20px] xs:text-[32px] group-focus-within:text-warning
                     "
                 >
                     Interactive Map Explorer
                 </div>
                 {/* content */}
-                <div className="grid grid-cols-1 z-4 h-full group-hover:delay-400 border group-focus-within:opacity-100 opacity-0 duration-500 group-hover:opacity-100 text-white group-hover:text-warning">
+                <div className="grid grid-cols-1 z-4 h-full group-hover:delay-400 group-focus-within:opacity-100 opacity-0 duration-500 group-hover:opacity-100 text-white group-focus-within:text-warning group-hover:text-warning">
                     <div></div>
                     <div></div>
                     <div className="duration-1000 text-center pb-4 flex flex-col gap-5 justify-end">
                         <p className="text-3xl xs:text-5xl font-bold">Interactive Map Explorer</p>
                         <p className="text-white text-[.7rem] xs:text-base">Kenali sosok di balik aplikasi ini—latar belakang, keahlian, dan visi pembuatan proyek.</p>
-                        <button className="text-sm w-full p-2 xs:p-4 rounded-box text-white mt-0 ring-3 ring-gray-50 cursor-pointer hover:bg-white hover:text-black">
+                        <button className="text-sm w-full p-2 xs:p-4 rounded-box text-white mt-0 ring-3 ring-gray-50 cursor-pointer focus-within:bg-white focus-within:text-black hover:bg-white hover:text-black">
                             Explore
                         </button>
                     </div>
