292fccca9ebedce53ed2b2a5685195b9642224dc
ahmadadptr001 <ahmadadptr@gmail.com>
2025-06-27 09:02:36 +0700

    perbaikan ui/ux sidebar bottom

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,13 +16,13 @@ const Sidebar = () => {
         const handleScroll = () => {
             const currentScroll = window.scrollY;
             if (currentScroll < lastScroll) {
-                setShowSidebar(false);
-            } else {
                 setShowSidebar(true);
+            } else {
+                setShowSidebar(false);
             }
 
             setLastScroll(currentScroll);
-            console.log("berhasil di set")
+            console.log("berhasil di set");
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -33,7 +33,7 @@ const Sidebar = () => {
     return (
         <section
             id="sidebar-right"
-            className={`xs:h-screen fixed ${showSidebar ? "bottom-0" : "-bottom-24"} transition-all duration-500 z-20 xs:sticky xs:top-0 w-full xs:w-auto`}
+            className={`xs:h-screen fixed ${showSidebar ? "bottom-0" : "-bottom-30"} transition-all duration-500 z-20 xs:sticky xs:top-0 w-full xs:w-auto`}
         >
             <div className="shdaow-md rounded-xl xs:xs:w-18 w-full bg-base-300 xs:bg-base-100 p-4 xs:h-full flex xs:flex-col gap-1 text-[.6rem] items-end xs:items-center justify-around xs:justify-start">
                 <div className="hidden xs:block">
@@ -41,7 +41,7 @@ const Sidebar = () => {
                 </div>
                 <NavLink
                     to="/maps"
-                    className="flex xs:mt-10 rounded-full xs:rounded-none flex-col gap-1 xs:w-18 hover:bg-accent items-center hover:cursor-pointer p-4 pb-3 xs:pb-4"
+                    className={({isActive}) => `flex xs:mt-10 rounded-full xs:rounded-none flex-col gap-1 xs:w-18 ${isActive ? 'shadow-accent shadow-lg/70' : ''} hover:bg-accent items-center hover:cursor-pointer p-4 pb-3 xs:pb-4`}
                     style={({ isActive }) => ({ background: isActive ? "var(--color-accent)" : "transparent" })}
                 >
                     <LiaMapSolid className="size-6" />
@@ -49,7 +49,12 @@ const Sidebar = () => {
                 </NavLink>
                 <NavLink
                     to="/weather"
-                    className="flex flex-col  items-center gap-1 xs:outline-none xs:hover:bg-accent xs:w-18 xs:hover:scale-100 xs:rounded-none xs:hover:shadow-none outline-2 p-4 rounded-full hover:shadow-accent hover:shadow-lg/70 cursor-pointer hover:scale-101"
+                    className={({ isActive }) =>
+                        `flex flex-col  items-center gap-1 xs:outline-none xs:hover:bg-accent
+                        xs:w-18 xs:hover:scale-100 xs:rounded-none xs:hover:shadow-none outline-2 p-4 rounded-full ${
+                            isActive ? "shadow-accent shadow-lg/70" : ""
+                        } cursor-pointer hover:scale-101`
+                    }
                     style={({ isActive }) => ({ background: isActive ? "var(--color-accent)" : "transparent" })}
                 >
                     <TiWeatherDownpour className="size-7" />
@@ -57,7 +62,7 @@ const Sidebar = () => {
                 </NavLink>
                 <NavLink
                     to="/about-me"
-                    className="flex rounded-full xs:rounded-none flex-col gap-1 xs:w-18 hover:bg-accent items-center hover:cursor-pointer p-4 pb-3 xs:pb-4"
+                    className={({isActive}) => `flex rounded-full xs:rounded-none flex-col gap-1 ${isActive ? "shadow-accent shadow-lg/70" : ""} xs:w-18 hover:bg-accent items-center hover:cursor-pointer p-4 pb-3 xs:pb-4`}
                     style={({ isActive }) => ({ background: isActive ? "var(--color-accent)" : "transparent" })}
                 >
                     <HiInformationCircle className="size-6" />
